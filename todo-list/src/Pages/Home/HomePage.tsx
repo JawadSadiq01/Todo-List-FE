715047815ce3d4eec0fa545db189851f7d8e306e
mehmood <mehmmod929@gmail.com>
2023-06-16 12:41:56 +0500

    Blog Page added!

--- todo-list/src/Pages/Home/HomePage.tsx
+++ todo-list/src/Pages/Home/HomePage.tsx
@@ -4,6 +4,7 @@ function HomePage() {
   return (
     <div className="bg-white-900 pt-5 pb-5 min-h-screen flex flex-col justify-center items-center">
       <TodoCard />
+      <a href="/blog" className="mt-10 inline-block py-2 px-4 bg-gray-800 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700">Wanna see the Blog!</a>
     </div>
   );
 }
